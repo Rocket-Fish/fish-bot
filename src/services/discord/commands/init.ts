c4ba5abdd1be69006d4377a25009e3572d9b5924
rocketfish <rocket.fish@mail.com>
2022-08-01 14:14:18 -0400

    wording adjustment

--- src/services/discord/commands/init.ts
+++ src/services/discord/commands/init.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { ApplicationCommand, ApplicationCommandTypes } from '../types';
-import { getGuildByDiscordId, createGuild } from '../../../models/Guild';
+import { getGuildByDiscordId, createGuild, Guild } from '../../../models/Guild';
 import { InteractionResponseType } from 'discord-interactions';
 import migrateGuildCommands from '../migrateGuildCommands';
 import { CONFIGURE_ROLE_BY_ZONE } from './configure-role-by-zone';
@@ -17,18 +17,30 @@ export const INIT: ApplicationCommand = {
 export async function handleInit(req: Request, res: Response) {
     const { guild_id } = req.body;
 
-    // check if guild exists in database already
-    const guild = await getGuildByDiscordId(guild_id);
+    let guild: Guild | undefined = undefined;
+    try {
+        // check if guild exists in database already
+        guild = await getGuildByDiscordId(guild_id);
+        // TODO: make this configurable later
+        await migrateGuildCommands(guild_id, [TEST, CONFIGURE_ROLE_BY_ZONE]);
+    } catch (e) {
+        return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: `Initialization failed \n> ${e}` },
+        });
+    }
+
     if (guild) {
         return res.send({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: { content: 'Server already initialized, you do not need to initialize it again.' },
+            data: {
+                content:
+                    'Server recognized as already initialized, running this command further will only update slash commands installed on this server',
+            },
         });
     } else {
         try {
             await createGuild(guild_id);
-            // TODO: make this configurable later
-            await migrateGuildCommands(guild_id, [TEST, CONFIGURE_ROLE_BY_ZONE]);
             return res.send({
                 type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 data: { content: 'Initialization complete' },
